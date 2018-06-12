bcc15f2c6b10b6018e647d8a33e32a508aba214c
hlz <hlz@bslab.org>
2018-06-12 16:26:47 +0800

    Initial commit

---
+++ app/src/main/java/cn/nitsc/hlz/alipaytransport/HookTrans.java
@@ -0,0 +1,18 @@
+package cn.nitsc.hlz.alipaytransport;
+
+import java.lang.reflect.Field;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+public class HookTrans implements IXposedHookLoadPackage {
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) {
+        if (lpparam.packageName.equals("com.eg.android.AlipayGphone")) {
+
+        }
+    }
+}
